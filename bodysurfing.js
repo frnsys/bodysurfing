7600df64262e070e5afb85db8f918bb19c635273
Francis Tseng <f+accounts@frnsys.com>
2016-02-06 16:41:18 -0500

    more links

--- bodysurfing.js
+++ bodysurfing.js
@@ -18,14 +18,39 @@ var highArousalSites = [
   "https://www.youtube.com/watch?v=3tjoqhx_dwk",
   "https://www.irs.gov/Affordable-Care-Act/Individuals-and-Families/Affordable-Care-Act--What-to-Expect-when-Filing-Your-2015-Tax-Return",
   "http://www.timeout.com/newyork/blog/map-of-average-rent-by-nyc-neighborhood-is-as-depressing-as-youd-expect-082115",
-  "http://www.cnbc.com/2016/02/05/citi-world-economy-trapped-in-death-spiral.html"
+  "http://www.cnbc.com/2016/02/05/citi-world-economy-trapped-in-death-spiral.html",
+  "https://i.imgur.com/BabIjym.jpg",
+  "https://i.imgur.com/GWS3l3l.jpg",
+  "http://companiesmobilizingcustomers.tumblr.com/post/131664566292/airbnb-ad-campaign-as-part-of-8m-campaign",
+  "http://saddesklunch.com/image/131102057774",
+  "https://www.youtube.com/watch?v=PvbL_5rH1QQ",
+  "https://www.youtube.com/watch?v=5IXQ6f6eMxQ",
+  "https://www.youtube.com/watch?v=NP7hfYunoeU",
+  "https://www.youtube.com/watch?v=R1uHk7Z6z24",
+  "https://youtu.be/gLDYtH1RH-U?t=167"
 ];
 var lowArousalSites = [
   "http://www.lookingatsomething.com/",
   "http://www.calm.com/",
   "http://www.donothingfor2minutes.com/",
   "http://www.deepthoughtsbyjackhandey.com/category/deepthoughts/",
-  "https://www.youtube.com/watch?v=kZr8sR9Gwag"
+  "https://www.youtube.com/watch?v=kZr8sR9Gwag",
+  "http://www.nytimes.com/2016/02/06/science/in-a-slovenian-cave-hoping-for-a-batch-of-baby-dragons.html?contentCollection=weekendreads&action=click&pgtype=Homepage&clickSource=image&module=c-column-middle-span-region&region=c-column-middle-span-region&WT.nav=c-column-middle-span-region&_r=0",
+  "http://images.realclear.com/290459_5_.jpg",
+  "https://www.youtube.com/watch?v=XZmdzfvXRuw",
+  "https://www.youtube.com/watch?v=D-UmfqFjpl0",
+  "http://www.barcinski-jeanjean.com/entries/line3d/",
+  "http://www.fallingfalling.com/",
+  "http://spielzeugz.de/html5/liquid-particles.html",
+  "https://www.youtube.com/watch?v=oKXVmzod-TE",
+  "http://45.media.tumblr.com/0777294e5cb89a26372d815b3cf79adf/tumblr_o0jhwwAgbQ1qzw1qyo1_540.gif",
+  "http://49.media.tumblr.com/a76d8f41afd6f2bb78c1cebbb03d1bcb/tumblr_nsix0i3wCZ1qzw1qyo1_540.gif",
+  "http://49.media.tumblr.com/c1d54d06da841f3c943783e95c45073b/tumblr_nao0z4k3zO1qzw1qyo1_500.gif",
+  "http://45.media.tumblr.com/7ed2dab49586c00956a0bb491d1b5538/tumblr_mlnzx84Ur21qzw1qyo1_500.gif",
+  "http://49.media.tumblr.com/a31cf073e7507ed79617aabab9c291c2/tumblr_mjpuzqHPYU1qzw1qyo1_500.gif",
+  "http://45.media.tumblr.com/99a6e3947ce261ed69bb1718c06bed6e/tumblr_mh94l03PX11qzw1qyo1_500.gif",
+  "http://49.media.tumblr.com/34d0f835a1841bcb01e38c6f1a7e4ae4/tumblr_mgwafqL33c1qzw1qyo1_500.gif",
+  "https://vimeo.com/53444911"
 ];
 
 // redirects the current tab to the specified url
